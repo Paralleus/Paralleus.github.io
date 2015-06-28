64555364fb8e979378bcf6dc08c009303ccdae5b
Paralleus <makotoonw@gmail.com>
2015-06-29 09:34:59 +1000

    Update paralleus.js

--- js/paralleus.js
+++ js/paralleus.js
@@ -28,12 +28,12 @@ function load_pages(){
             {
                 var pages = String(rawFile.responseText).split("\n");
                 var toolbar = "";
-                var page = "|&nbsp;&nbsp;";
+                var page = "|";
 		for(i=0;i<pages.length;i+=3){
 			if(pages[i+1].indexOf("URL:") > -1){
-				toolbar += "<a href='#"+pages[i+1]+"'>"+pages[i]+"</a>&nbsp;&nbsp;|"
+				toolbar += "&nbsp;&nbsp;<a href='#"+pages[i+1]+"'>"+pages[i]+"</a>&nbsp;&nbsp;|"
 			} else {
-				toolbar += "<a href='#"+pages[i]+"'>"+pages[i]+"</a>&nbsp;&nbsp;|"
+				toolbar += "&nbsp;&nbsp;<a href='#"+pages[i]+"'>"+pages[i]+"</a>&nbsp;&nbsp;|"
 			}
 		}
 		document.getElementById('toolbar').innerHTML = toolbar
