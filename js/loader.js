8ab99374f4768a453effd48e807fe25986957e7a
Paralleus <makotoonw@gmail.com>
2015-06-29 11:48:21 +1000

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,2 +1,1 @@
 $(window).load(function(){$(".loader").fadeOut("slow");})
-document.head.innerHTML += "<style>.loader {position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url('http://media.giphy.com/media/147upcc99OCGOI/giphy.gif') 50% 50%	no-repeat rgb(255,255,255);}</style>";
