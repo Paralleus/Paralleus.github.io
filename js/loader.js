5a9913fd2d01ef16fbfe07bf4691d1e2435540b4
Paralleus <makotoonw@gmail.com>
2015-06-29 11:20:03 +1000

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+})
+document.getElementById("loader").style.position="fixed";
+document.getElementById("loader").style.left="0px";
+document.getElementById("loader").style.top="0px";
+document.getElementById("loader").style.width="100%";
+document.getElementById("loader").style.height="100%";
+document.getElementById("loader").style.z-index="100";
+document.getElementById("loader").style.background="url('http://media.giphy.com/media/147upcc99OCGOI/giphy.gif') 50% 50% 	no-repeat rgb(255,255,255);";
