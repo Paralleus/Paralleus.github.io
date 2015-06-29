0e23cd08b0fb69b1ab06df65f7ac6b14154ecc83
Paralleus <makotoonw@gmail.com>
2015-06-29 11:43:27 +1000

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,2 +1,2 @@
 $(window).load(function(){$(".loader").fadeOut("slow");})
-document.body.innerHTML += "<style>.loader {position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url('http://media.giphy.com/media/147upcc99OCGOI/giphy.gif') 50% 50%	no-repeat rgb(255,255,255);}</style>";
+document.head.innerHTML += "<style>.loader {position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url('http://media.giphy.com/media/147upcc99OCGOI/giphy.gif') 50% 50%	no-repeat rgb(255,255,255);}</style>";
