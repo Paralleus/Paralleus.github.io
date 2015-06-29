fd3adeeb27a1a53c4d1c184d38252f9324f04c5c
Paralleus <makotoonw@gmail.com>
2015-06-29 10:49:23 +1000

    Update initialize.js

--- js/initialize.js
+++ js/initialize.js
@@ -14,12 +14,10 @@ function loadjscssfile(filename, filetype){
         document.getElementsByTagName("head")[0].appendChild(fileref)
 }
  
-function loadmain(){
-    loadjscssfile("js/scroller/jquery.mCustomScrollbar.css?time="+Date.now(), "css")
-    loadjscssfile("css/style.css?time="+Date.now(), "css")
-    loadjscssfile("http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", "js")
-    loadjscssfile("js/paralleus.js?time="+Date.now(), "js")
-    loadjscssfile("js/scroller/jquery.mCustomScrollbar.concat.min.js?time="+Date.now(), "js")
-    loadjscssfile("js/load_scroller.js?time="+Date.now(), "js")
-}
+loadjscssfile("js/scroller/jquery.mCustomScrollbar.css?time="+Date.now(), "css")
+loadjscssfile("css/style.css?time="+Date.now(), "css")
+loadjscssfile("http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", "js")
+loadjscssfile("js/paralleus.js?time="+Date.now(), "js")
+loadjscssfile("js/scroller/jquery.mCustomScrollbar.concat.min.js?time="+Date.now(), "js")
+loadjscssfile("js/load_scroller.js?time="+Date.now(), "js")
 
