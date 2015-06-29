1406682cd713ea5bc781bc2ffab7072148eb3cff
Paralleus <makotoonw@gmail.com>
2015-06-29 11:48:29 +1000

    Update initialize.js

--- js/initialize.js
+++ js/initialize.js
@@ -15,7 +15,6 @@ function loadjscssfile(filename, filetype){
 }
 loadjscssfile("css/style.css?time="+Date.now(), "css")
 loadjscssfile("http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js","js");
-loadjscssfile("js/loader.js","js")
 loadjscssfile("http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", "js")
 loadjscssfile("js/scroller/jquery.mCustomScrollbar.css?time="+Date.now(), "css")
 loadjscssfile("js/paralleus.js?time="+Date.now(), "js")
